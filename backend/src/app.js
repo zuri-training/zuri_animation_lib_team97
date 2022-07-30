de99a556f2c3c290b565d522e32aa87f5dbfa3e2
Isaac Awonugba <78800584+afroisaac@users.noreply.github.com>
2022-07-30 17:08:36 +0100

    Express added

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 const http = require('http');
-const routes = require('./routes');
+const express = require('express');
 
-const server = http.createServer(routes);
+const app = express();
+
+const server = http.createServer(app);
 
 server.listen(3000);
